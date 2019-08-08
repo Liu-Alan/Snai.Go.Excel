4137b08f4f409b01bbe05f18a6c2b599b0f4d414
小林 刘 <xiaolin-liu@qq.com>
2019-08-08 19:05:16 +0800

    excel 读取操作

--- main.go
+++ main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	"Snai.Go.Excel/entities"
 	"Snai.Go.Excel/utils"
 )
 
-func main(){
-	files, err := ioutil.ReadDir("excel")
+func main() {
+	dirPath := "excel"
+	files, err := ioutil.ReadDir(dirPath)
 
-	if err != nil{
-        return
+	if err != nil {
+		return
 	}
-	
-	for _, file := range files{
-        if !strings.Contains(file.Name, ".xls") && !strings.Contains(file.Name, ".xlsx"){
-            continue
+
+	var orderList []entities.Order
+	for _, file := range files {
+		if strings.Index(file.Name(), ".xlsx") < 0 {
+			continue
 		}
-		
-    }
+
+		orders := utils.ExcelRead(dirPath + "/" + file.Name())
+
+		if len(orders) > 0 {
+			orderList = append(orderList, orders...)
+		}
+	}
+
+	fmt.Printf("len:%d", len(orderList))
+	if len(orderList) > 0 {
+		for _, value := range orderList {
+			fmt.Printf("JobNo:%s,Qyt:%s", value.JobNo, value.Qyt)
+		}
+	}
 }
